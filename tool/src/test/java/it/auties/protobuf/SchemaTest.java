cd90437f7f198eedd2de0b6561e93fea2e40708e
Alessandro Autiero <alautiero@gmail.com>
2022-12-28 17:46:58 +0100

    Fixed unit test for tool

--- tool/src/test/java/it/auties/protobuf/SchemaTest.java
+++ tool/src/test/java/it/auties/protobuf/SchemaTest.java
@@ -1,21 +1,23 @@
 package it.auties.protobuf;
 
-import it.auties.protobuf.tool.application.ProtobufApplication;
+import it.auties.protobuf.tool.command.BaseCommand;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import picocli.CommandLine;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
 
 public class SchemaTest {
     @Test
-    public void testSchemaGeneration() throws IOException, URISyntaxException {
-        var source = ClassLoader.getSystemClassLoader().getResource("whatsapp.proto");
-        Objects.requireNonNull(source);
+    public void testSchemaGeneration() {
+        Assertions.assertDoesNotThrow(() -> {
+            var source = ClassLoader.getSystemClassLoader().getResource("whatsapp.proto");
+            Objects.requireNonNull(source);
 
-        var folder = Files.createTempDirectory("output");
-        ProtobufApplication.main("generate", Path.of(source.toURI()).toString(), "--output", folder.toString());
+            var folder = Files.createTempDirectory("output");
+            new CommandLine(new BaseCommand()).execute("generate", Path.of(source.toURI()).toString(), "--output", folder.toString());
+        });
     }
 }
