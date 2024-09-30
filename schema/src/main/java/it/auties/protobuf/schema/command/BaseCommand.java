ae12f0d04b11a69e2c513ac0d6addcc99675d425
Alessandro Autiero <alautiero@gmail.com>
2024-09-30 11:52:04 +0200

    <release: 3.4.1>

--- schema/src/main/java/it/auties/protobuf/schema/command/BaseCommand.java
+++ schema/src/main/java/it/auties/protobuf/schema/command/BaseCommand.java
@@ -4,7 +4,7 @@ import picocli.CommandLine.Command;
 
 @Command(
         mixinStandardHelpOptions = true,
-        version = "ModernProtobuf 3.4.0",
+        version = "ModernProtobuf 3.4.1",
         description = "A custom implementation of protobuf written in Java 17",
         subcommands = {GenerateCommand.class, UpdateCommand.class}
 )
